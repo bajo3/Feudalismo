f58f853bc84b892556051bafe371aac1c8525436
bajo3 <Felipelentini98@hotmail.com>
2023-09-01 14:06:00 -0300

    Menu 1 x 1

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.getElementById("menuToggle");
     const sidebar = document.getElementById("sidebar");
     const accountMenu = document.getElementById("accountMenu");
     const closeButton = document.getElementById("closeButton");
 
-    menuToggle.addEventListener("click", function() {
+    // Función para abrir el menú de cuenta y cerrar el menú lateral
+    function openAccountMenu() {
+        accountMenu.classList.add("active");
+        sidebar.style.left = "-250px"; // Cierra el menú lateral
+    }
+
+    // Función para cerrar el menú de cuenta
+    function closeAccountMenu() {
+        accountMenu.classList.remove("active");
+    }
+
+    // Función para abrir o cerrar el menú lateral según su estado actual
+    function toggleSidebar() {
         if (sidebar.style.left === "-250px") {
             sidebar.style.left = "0";
         } else {
             sidebar.style.left = "-250px";
         }
-    });
+        closeAccountMenu(); // Cierra el menú de cuenta al abrir el menú lateral
+    }
 
+    // Agregar evento de clic al botón del menú lateral
+    const menuToggle = document.getElementById("menuToggle");
+    menuToggle.addEventListener("click", toggleSidebar);
+
+    // Agregar evento de clic al botón de cuenta
     const account = document.querySelector(".account");
-    account.addEventListener("click", function() {
-        accountMenu.classList.toggle("active");
-    });
+    account.addEventListener("click", openAccountMenu);
 
-    // Cerrar el menú al hacer clic en el botón de cierre
-    closeButton.addEventListener("click", function() {
-        accountMenu.classList.remove("active");
-    });
+    // Agregar evento de clic al botón de cierre del menú de cuenta
+    closeButton.addEventListener("click", closeAccountMenu);
 
-    // Cerrar el menú al hacer clic fuera de él
+    // Cerrar el menú de cuenta al hacer clic fuera de él
     document.addEventListener("click", function(event) {
         if (!account.contains(event.target)) {
-            accountMenu.classList.remove("active");
+            closeAccountMenu();
         }
     });
+
     const darkModeToggle = document.getElementById("darkModeToggle");
     const body = document.body;
     let isDarkMode = false;
@@ -40,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function() {
         darkModeToggle.classList.toggle("dark", isDarkMode);
     }
 
-    // Cambia el modo y gira el botón al hacer clic en el botón de Modo Oscuro
+    // Cambia el modo y gira el botón al hacer clic en el botón de Modo Osc uro
     darkModeToggle.addEventListener("click", function() {
         toggleDarkMode();
     });
